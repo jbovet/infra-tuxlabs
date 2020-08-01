ff43a58f451d749f99620df6b22066cad4968820
Jose Bovet Derpich <jose.bovet@gmail.com>
2020-07-31 20:28:02 -0400

    remove domain

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,17 +3,3 @@ provider "linode" {
   version = "~> 1.12.4"
   token   = var.token
 }
-
-resource "linode_domain" "tuxlabs-domain" {
-  type      = "master"
-  domain    = "tuxlabs.io"
-  soa_email = "jp@tuxlabs.io"
-  tags      = ["tuxlabs", "labs"]
-}
-
-# resource "linode_domain_record" "tuxlabs-record" {
-#   domain_id   = "${linode_domain.tuxlabs-domain.id}"
-#   name        = "www"
-#   record_type = "CNAME"
-#   target      = "foobar.example"
-# }
