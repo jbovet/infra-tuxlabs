5d789a1cfdd110ac5c169d73453cec0fe1fc05fb
Jose Bovet Derpich <jose.bovet@gmail.com>
2020-07-31 20:57:08 -0400

    add k8s nodes

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,45 @@ provider "cloudflare" {
   api_key = var.cloudflare_api_key
 }
 
+//Use the linode_lke_cluster resource to create
+//a Kubernetes cluster
+resource "linode_lke_cluster" "tuxlabs" {
+  k8s_version = var.k8s_version
+  label       = var.label
+  region      = var.region
+  tags        = var.tags
+
+  dynamic "pool" {
+    for_each = var.pools
+    content {
+      type  = pool.value["type"]
+      count = pool.value["count"]
+    }
+  }
+}
+
+//Export this cluster's attributes
+output "kubeconfig" {
+  value     = linode_lke_cluster.tuxlabs.kubeconfig
+  sensitive = true
+}
+
+output "api_endpoints" {
+  value = linode_lke_cluster.tuxlabs.api_endpoints
+}
+
+output "status" {
+  value = linode_lke_cluster.tuxlabs.status
+}
+
+output "id" {
+  value = linode_lke_cluster.tuxlabs.id
+}
+
+output "pool" {
+  value = linode_lke_cluster.tuxlabs.pool
+}
+
 resource "cloudflare_record" "www" {
   zone_id = var.cloudflare_zone_id
   name    = "www"
