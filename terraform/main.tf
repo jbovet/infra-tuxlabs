725c1d00d7f58bd0ce671f33e059583fb68c2c43
Jose Bovet Derpich <jose.bovet@gmail.com>
2020-08-09 14:29:45 -0400

    feat(lb): destroy

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,38 +12,38 @@ provider "cloudflare" {
 
 //Use the linode_lke_cluster resource to create
 //a Kubernetes cluster
-resource "linode_lke_cluster" "tuxlabs" {
-  k8s_version = var.k8s_version
-  label       = var.label
-  region      = var.region
-  tags        = var.tags
-
-  dynamic "pool" {
-    for_each = var.pools
-    content {
-      type  = pool.value["type"]
-      count = pool.value["count"]
-    }
-  }
-}
-
-resource "linode_nodebalancer" "tuxlabs_lb" {
-  label                = var.label
-  region               = var.region
-  tags                 = var.tags
-  client_conn_throttle = 20
-}
-
-resource "linode_nodebalancer_config" "tuxlabs_lb_config" {
-  nodebalancer_id = linode_nodebalancer.tuxlabs_lb.id
-  port            = 443
-  protocol        = "https"
-  check           = "http"
-  check_path      = "/ping"
-  check_attempts  = 3
-  check_timeout   = 5
-  algorithm       = "source"
-}
+# resource "linode_lke_cluster" "tuxlabs" {
+#   k8s_version = var.k8s_version
+#   label       = var.label
+#   region      = var.region
+#   tags        = var.tags
+#
+#   dynamic "pool" {
+#     for_each = var.pools
+#     content {
+#       type  = pool.value["type"]
+#       count = pool.value["count"]
+#     }
+#   }
+# }
+#
+# resource "linode_nodebalancer" "tuxlabs_lb" {
+#   label                = var.label
+#   region               = var.region
+#   tags                 = var.tags
+#   client_conn_throttle = 20
+# }
+#
+# resource "linode_nodebalancer_config" "tuxlabs_lb_config" {
+#   nodebalancer_id = linode_nodebalancer.tuxlabs_lb.id
+#   port            = 443
+#   protocol        = "https"
+#   check           = "http"
+#   check_path      = "/ping"
+#   check_attempts  = 3
+#   check_timeout   = 5
+#   algorithm       = "source"
+# }
 
 # resource "linode_nodebalancer_node" "tuxlabs_lb_node" {
 #   count           = "3"
@@ -55,32 +55,32 @@ resource "linode_nodebalancer_config" "tuxlabs_lb_config" {
 # }
 
 //DNS
-resource "cloudflare_record" "www" {
-  zone_id = var.cloudflare_zone_id
-  name    = "www"
-  value   = linode_nodebalancer.tuxlabs_lb.ipv4
-  type    = "A"
-  ttl     = 1
-}
+# resource "cloudflare_record" "www" {
+#   zone_id = var.cloudflare_zone_id
+#   name    = "www"
+#   value   = linode_nodebalancer.tuxlabs_lb.ipv4
+#   type    = "A"
+#   ttl     = 1
+# }
 
 //Export this cluster's attributes
-output "kubeconfig" {
-  value     = linode_lke_cluster.tuxlabs.kubeconfig
-  sensitive = true
-}
-
-output "api_endpoints" {
-  value = linode_lke_cluster.tuxlabs.api_endpoints
-}
-
-output "status" {
-  value = linode_lke_cluster.tuxlabs.status
-}
-
-output "id" {
-  value = linode_lke_cluster.tuxlabs.id
-}
-
-output "pool" {
-  value = linode_lke_cluster.tuxlabs.pool
-}
+# output "kubeconfig" {
+#   value     = linode_lke_cluster.tuxlabs.kubeconfig
+#   sensitive = true
+# }
+#
+# output "api_endpoints" {
+#   value = linode_lke_cluster.tuxlabs.api_endpoints
+# }
+#
+# output "status" {
+#   value = linode_lke_cluster.tuxlabs.status
+# }
+#
+# output "id" {
+#   value = linode_lke_cluster.tuxlabs.id
+# }
+#
+# output "pool" {
+#   value = linode_lke_cluster.tuxlabs.pool
+# }
