70fcca1eaaaf97f11be82141fad1a73597b9d3ee
Jose Bovet Derpich <jose.bovet@gmail.com>
2020-08-09 15:37:14 -0400

    feat(lb): add lb node balancer

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,7 +49,7 @@ resource "linode_nodebalancer_node" "tuxlabs_lb_node" {
   count           = "3"
   nodebalancer_id = linode_nodebalancer.tuxlabs_lb.id
   config_id       = linode_nodebalancer_config.tuxlabs_lb_config.id
-  address         = "${element(linode_lke_cluster.tuxlabs.pool.*.private_ip_address, count.index)}:80"
+  address         = "${element(linode_lke_cluster.tuxlabs.pool.*.nodes.private_ip_address, count.index)}:80"
   label           = var.label
   weight          = 50
 }
