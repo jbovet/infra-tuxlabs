3caa03cf2f5055d9de0e5fa492729cf9c32771da
Jose Bovet Derpich <jose.bovet@gmail.com>
2020-08-09 14:05:46 -0400

    feat(lb): add lb node balancer

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,6 +27,42 @@ resource "linode_lke_cluster" "tuxlabs" {
   }
 }
 
+resource "linode_nodebalancer" "tuxlabs_lb" {
+  label                = var.label
+  region               = var.region
+  tags                 = var.tags
+  client_conn_throttle = 20
+}
+
+resource "linode_nodebalancer_config" "tuxlabs_lb_config" {
+  nodebalancer_id = linode_nodebalancer.tuxlabs_lb.id
+  port            = 443
+  protocol        = "https"
+  check           = "http"
+  check_path      = "/ping"
+  check_attempts  = 3
+  check_timeout   = 5
+  algorithm       = "source"
+}
+
+# resource "linode_nodebalancer_node" "tuxlabs_lb_node" {
+#   count           = "3"
+#   nodebalancer_id = linode_nodebalancer.tuxlabs_lb.id
+#   config_id       = linode_nodebalancer_config.tuxlabs_lb_config.id
+#   address         = "${element(linode_instance.web.*.private_ip_address, count.index)}:80"
+#   label           = var.label
+#   weight          = 50
+# }
+
+//DNS
+resource "cloudflare_record" "www" {
+  zone_id = var.cloudflare_zone_id
+  name    = "www"
+  value   = linode_nodebalancer.tuxlabs_lb.ipv4
+  type    = "A"
+  ttl     = 1
+}
+
 //Export this cluster's attributes
 output "kubeconfig" {
   value     = linode_lke_cluster.tuxlabs.kubeconfig
@@ -47,12 +83,4 @@ output "id" {
 
 output "pool" {
   value = linode_lke_cluster.tuxlabs.pool
-}
-
-resource "cloudflare_record" "www" {
-  zone_id = var.cloudflare_zone_id
-  name    = "www"
-  value   = "192.168.0.11"
-  type    = "A"
-  ttl     = 1
 }
