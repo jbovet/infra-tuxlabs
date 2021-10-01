4e140327ec9a7042ed7e2bc991837aca5e4963fc
Jose Bovet Derpich <jose.bovet@gmail.com>
2021-10-01 20:23:59 -0300

    test linode_nodebalancer_node

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,14 +56,15 @@ resource "linode_nodebalancer_config" "tuxlabs_lb_config" {
   algorithm       = "source"
 }
 
-# resource "linode_nodebalancer_node" "tuxlabs_lb_node" {
-#   count           = "3"
-#   nodebalancer_id = linode_nodebalancer.tuxlabs_lb.id
-#   config_id       = linode_nodebalancer_config.tuxlabs_lb_config.id
-#   address         = "${element(linode_lke_cluster.tuxlabs.pool.*.nodes.private_ip_address, count.index)}:80"
-#   label           = var.label
-#   weight          = 50
-# }
+resource "linode_nodebalancer_node" "tuxlabs_lb_node" {
+  # count           = "2"
+  nodebalancer_id = linode_nodebalancer.tuxlabs_lb.id
+  config_id       = linode_nodebalancer_config.tuxlabs_lb_config.id
+  # address         = "${element(linode_lke_cluster.tuxlabs.pool.*.nodes.private_ip_address, count.index)}:80"
+  address         = "lke30564667-63033-615792fe23a8"
+  label           = var.label
+  weight          = 50
+}
 
 //DNS
 # resource "cloudflare_record" "www" {
