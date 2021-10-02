42174db3d9c6846e43ccf61e92a4677dfe7d14a1
Jose Bovet Derpich <jose.bovet@gmail.com>
2021-10-02 18:00:11 -0300

    feat(lke): create cluster and load balancer config

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,66 +22,66 @@ provider "cloudflare" {
 
 //Use the linode_lke_cluster resource to create
 //a Kubernetes cluster
-# resource "linode_lke_cluster" "tuxlabs" {
-#   k8s_version = var.k8s_version
-#   label       = var.label
-#   region      = var.region
-#   tags        = var.tags
+resource "linode_lke_cluster" "tuxlabs" {
+  k8s_version = var.k8s_version
+  label       = var.label
+  region      = var.region
+  tags        = var.tags
 
-#   dynamic "pool" {
-#     for_each = var.pools
-#     content {
-#       type  = pool.value["type"]
-#       count = pool.value["count"]
-#     }
-#   }
-# }
+  dynamic "pool" {
+    for_each = var.pools
+    content {
+      type  = pool.value["type"]
+      count = pool.value["count"]
+    }
+  }
+}
 
-# resource "linode_nodebalancer" "tuxlabs_lb" {
-#   label                = var.label
-#   region               = var.region
-#   tags                 = var.tags
-#   client_conn_throttle = var.conn_throttle
-# }
+resource "linode_nodebalancer" "tuxlabs_lb" {
+  label                = var.label
+  region               = var.region
+  tags                 = var.tags
+  client_conn_throttle = var.conn_throttle
+}
 
-# resource "linode_nodebalancer_config" "tuxlabs_lb_config" {
-#   nodebalancer_id = linode_nodebalancer.tuxlabs_lb.id
-#   port            = 443
-#   protocol        = "http"
-#   check           = "http"
-#   check_path      = "/ping"
-#   check_attempts  = 3
-#   check_timeout   = 5
-#   check_interval  = 30
-#   algorithm       = "source"
-# }
+resource "linode_nodebalancer_config" "tuxlabs_lb_config" {
+  nodebalancer_id = linode_nodebalancer.tuxlabs_lb.id
+  port            = 443
+  protocol        = "http"
+  check           = "http"
+  check_path      = "/ping"
+  check_attempts  = 3
+  check_timeout   = 5
+  check_interval  = 30
+  algorithm       = "source"
+}
 
-# resource "linode_nodebalancer_node" "tuxlabs_lb_node" {
-#   count           = "2"
-#   nodebalancer_id = linode_nodebalancer.tuxlabs_lb.id
-#   config_id       = linode_nodebalancer_config.tuxlabs_lb_config.id
-#   address         = "${element(local.lke_node_ips, count.index)}:80"
-#   label           = var.label
-#   weight          = 50
-# }
+resource "linode_nodebalancer_node" "tuxlabs_lb_node" {
+  count           = "2"
+  nodebalancer_id = linode_nodebalancer.tuxlabs_lb.id
+  config_id       = linode_nodebalancer_config.tuxlabs_lb_config.id
+  address         = "${element(local.lke_node_ips, count.index)}:80"
+  label           = var.label
+  weight          = 50
+}
 
 # ## Data Sources
-# data "linode_instances" "tuxlabs" {
-#   filter {
-#     name   = "id"
-#     values = local.lke_node_ids
-#   }
-# }
+data "linode_instances" "tuxlabs" {
+  filter {
+    name   = "id"
+    values = local.lke_node_ids
+  }
+}
 
-# locals {
-#   lke_node_ids = flatten(
-#     [for pool in linode_lke_cluster.tuxlabs.pool :
-#       [for node in pool.nodes : node.instance_id]
-#     ]
-#   )
-#   lke_node_ip   = data.linode_instances.tuxlabs.instances[0].private_ip_address
-#   lke_node_ips  = data.linode_instances.tuxlabs.instances.*.private_ip_address
-# }
+locals {
+  lke_node_ids = flatten(
+    [for pool in linode_lke_cluster.tuxlabs.pool :
+      [for node in pool.nodes : node.instance_id]
+    ]
+  )
+  lke_node_ip   = data.linode_instances.tuxlabs.instances[0].private_ip_address
+  lke_node_ips  = data.linode_instances.tuxlabs.instances.*.private_ip_address
+}
 
 //DNS
 # resource "cloudflare_record" "www" {
@@ -93,23 +93,23 @@ provider "cloudflare" {
 # }
 
 //Export this cluster's attributes
-# output "kubeconfig" {
-#   value     = linode_lke_cluster.tuxlabs.kubeconfig
-#   sensitive = true
-# }
+output "kubeconfig" {
+  value     = linode_lke_cluster.tuxlabs.kubeconfig
+  sensitive = true
+}
 
-# output "api_endpoints" {
-#   value = linode_lke_cluster.tuxlabs.api_endpoints
-# }
+output "api_endpoints" {
+  value = linode_lke_cluster.tuxlabs.api_endpoints
+}
 
-# output "status" {
-#   value = linode_lke_cluster.tuxlabs.status
-# }
+output "status" {
+  value = linode_lke_cluster.tuxlabs.status
+}
 
-# output "id" {
-#   value = linode_lke_cluster.tuxlabs.id
-# }
+output "id" {
+  value = linode_lke_cluster.tuxlabs.id
+}
 
-# output "pool" {
-#   value = linode_lke_cluster.tuxlabs.pool
-# }
+output "pool" {
+  value = linode_lke_cluster.tuxlabs.pool
+}
